31740adf2585d652ed3ea75d91c6242c8f10ea9b
bullhornfixie <dawson.jon@hotmail.com>
2020-12-27 16:07:36 +0000

    Wrap code in methods

--- directory.rb
+++ directory.rb
@@ -1,14 +1,31 @@
 # Let's put all students into an array 
-students = [ "Dr. Hannibal Lecter", "Darth Vader", "Nurse Ratched",
-            "Michael Corleone", "Alex DeLarge", "The Wicked Witch of the West",
-            "Terminator", "Freddy Krueger", "The Joker", "Joffrey Baratheon",
-            "Norman Bates" ]
+students = [ 
+  ["Dr. Hannibal Lecter", :november],
+  ["Darth Vader", :november],
+  ["Nurse Ratched", :november],
+  ["Michael Corleone", :november],
+  ["Alex DeLarge", :november],
+  ["The Wicked Witch of the West", :november],
+  ["Terminator", :november],
+  ["Freddy Krueger", :november],
+  ["The Joker", :november],
+  ["Joffrey Baratheon", :november],
+  ["Norman Bates", :november]
+]
 
-# ane then print them
-puts "The students of Villains Academy"
-puts "-------------"
-students.each { |s| puts s }
+def print_header 
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
 
-# finally we print the total 
-puts "Overall, we have #{students.count} great students"
+def print_cohort(list)
+  list.each { |student| puts "#{student[0]} (#{student[1]} cohort)" }
+end
 
+def print_footer(names) 
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header 
+print_cohort(students)
+print_footer(students)
