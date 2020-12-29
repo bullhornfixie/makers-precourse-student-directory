24a6c3c7a43cf0bedf7044270f12751c2b8b6104
bullhornfixie <dawson.jon@hotmail.com>
2020-12-29 14:52:23 +0000

    Experimental refactoring

--- directory.rb
+++ directory.rb
@@ -1,46 +1,51 @@
 def input_students
-  puts "Please enter student name"
+  puts "Please enter student name and then cohort seperated by comma e.g. jon, october"
   puts "To finish, hit enter twice"
 
   students = []                                     
   
-  name = gets.chop
+  name_cohort = gets.chomp
  
-  while !name.empty? do 
-    students << {name: name, cohort: :november}
+  while !name_cohort.empty? do  
+    name, cohort = name_cohort.split(", ")
+    # split name_cohort into 2 variables
+    students << {name: name.capitalize, cohort: cohort.to_sym.capitalize}
+    # feed these two variables into a hash which is pushed to an array 
     puts students.count < 2 ? "Now we have #{students.count} student" : "Now we have #{students.count} students"
-    name = gets.chop
+    # tell me student count 
+    name_cohort = gets.chomp
+    # ask for another student name, cohort 
   end
-  
   students                                          
 end 
 
 def print_header
-  puts "The students of Villains Academy"
+  puts "The Students of Makers Academy"
   puts "-------------"
 end
 
 def print_cohort(students)
+  value_for_footer = 0 
+
   if students.count < 1
     puts "empty list!"
-    # only print list if at least 1 student
   else
-    group1 = students.select { |student| student[:cohort] == :november }
-    group2 = students.select { |student| student[:cohort] == :october }
-
-    align = lambda { |student| puts "#{student[:name]}".ljust(10) + "(#{student[:cohort]} cohort)".center(10) }
-
-    group1.each(&align)
-    group2.each(&align)
+    puts "Which cohort is required?"
+    input = gets.chomp
+    grouping = students.select { |student| student[:cohort] == input.to_sym.capitalize}
+    value_for_footer += grouping.count
+    grouping.each { |student| puts "#{student[:name]}".ljust(10) + "(#{student[:cohort]} cohort)".center(10) }
   end
+
+  value_for_footer
 end
 
-def print_footer(students)
-  wording = "Overall, we have #{students.count} great student"
-  puts students.count < 2 ? wording : wording + "s"
+def print_footer(value_for_footer)
+  wording = "Overall, we have #{value_for_footer} great student"
+  puts value_for_footer < 2 ? wording : wording + "s"
 end
 
-students = input_students                             
+students = input_students
 print_header 
-print_cohort(students)
-print_footer(students)
+value_for_footer = print_cohort(students)                             
+print_footer(value_for_footer)
