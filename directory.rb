18b1f1bb43792ba228b6da243cf089a991cc0e47
bullhornfixie <dawson.jon@hotmail.com>
2021-01-03 15:01:56 +0000

    Exercise14 - using Ruby's built-in CSV library

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+require "csv"
 @students = []
 
 def interactive_menu
@@ -17,16 +18,11 @@ end
 
 def process(selection)
   case selection 
-    when "1"
-      input_students 
-    when "2"
-      show_students
-    when "3"
-      save_students
-    when "4"
-      load_students_from_program
-    when "9"
-      puts "You have selected option 9: Exit"
+    when "1" then input_students 
+    when "2" then show_students
+    when "3" then save_students
+    when "4" then load_students
+    when "9" then puts "You have selected option 9: Exit"
       exit 
     else
       puts "I dont know what you meant, try again"
@@ -44,7 +40,7 @@ def input_students
     name, cohort = name_cohort.split(", ")
     student_entry(name, cohort)
     puts @students.count < 2 ? "Now we have #{@students.count} student" : "Now we have #{@students.count} students"
-    name_cohort = STDIN.gets.chomp # input next student from command line (Standard Input aka STDIN)
+    name_cohort = STDIN.gets.chomp 
   end                                       
 end
 
@@ -61,55 +57,29 @@ def print_header
 end
 
 def print_students_list
-  if @students.count < 1
-    puts "empty list!"
-  else
-    puts "Which cohort is required?"
-    input = STDIN.gets.chomp
-    grouping = @students.select { |student| student[:cohort] == input.to_sym.capitalize}
-    @cohort_size = grouping.count
-    grouping.each { |student| puts "#{student[:name]}".ljust(10) + "(#{student[:cohort]} cohort)".center(10) }
-  end
+  puts @students 
 end
 
 def print_footer
-  wording = "In this cohort, we have #{@cohort_size} great student"
-  puts @cohort_size < 2 ? wording : wording + "s"
+  wording = "In this cohort, we have #{@students.size} great student"
+  puts @students.size < 2 ? wording : wording + "s"
 end
-9
+
 def save_students
-  puts "You have selected option 3: Save the list to a file"
-  puts "What file would you like to save to? e.g. students.csv"
-  file_to_open = STDIN.gets.chomp
-  working_file = File.open(file_to_open, "w")
+  file = CSV.open("students.csv", "w")
  
   @students.each do |student| 
-    student_data = [student[:name]],[student[:cohort]] 
-    csv_line = student_data.join(",") 
-    working_file.puts csv_line # re-directing standard output to file instead of terminal 
+    csv_line = [student]
+    file.puts csv_line
   end
-  working_file.close 
+ file.close 
+ puts "Student file saved"
 end
 
-def load_students_from_program
-  puts "What file what you like to load? e.g. students.csv"
-
-  file_to_load = STDIN.gets.chomp
-  working_file = File.open(file_to_load, "r") do |f|
-    f.readlines.each { |line| name, cohort = line.chomp.split(",")
-    student_entry(name, cohort) } # feeding to method
-    puts "#{file_to_load} loaded"
-  end
-end
-
-def load_students_from_CMD
-  filename = ARGV.first # takes first argument passed via the command line
-  return if filename.nil? # exit method if no argument given 
-  if File.exists?(filename) # method used on File class
-    load_students_from_program(filename) # filename passed to load_students method
-      puts "Loaded #{@students.count} from #{filename}"
-  else # if it doesn't exist 
-    puts "Sorry, #{filename} doesn't exist."
+def load_students
+  file = CSV.open("students.csv", "r") do |csv_data|
+    csv_data.readlines.each { |line| @students << line.join }
+    puts "Student list loaded"
   end
 end
 
@@ -117,8 +87,7 @@ def student_entry(name, cohort)
   @students << {name: name.capitalize, cohort: cohort.to_sym.capitalize}
 end
 
-load_students_from_program
-load_students_from_CMD
+load_students
 interactive_menu
 
 
